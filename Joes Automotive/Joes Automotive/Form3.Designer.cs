4caefbd4952d6708ee9bcf4f09568ff9af1743e1
Anthony Green <32841570+GreenAnthony@users.noreply.github.com>
2018-03-21 06:42:53 -0700

    dsf
    sdf

---
+++ Joes Automotive/Joes Automotive/Form3.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Joes_Automotive
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customersSearch = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.workButton = new System.Windows.Forms.Button();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.saveButton1 = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.areaBox = new System.Windows.Forms.TextBox();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.phoneBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.customersBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.customersDataSet = new Joes_Automotive.customersDataSet();
+            this.customersTableAdapter = new Joes_Automotive.customersDataSetTableAdapters.CustomersTableAdapter();
+            this.tableAdapterManager = new Joes_Automotive.customersDataSetTableAdapters.TableAdapterManager();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.vehiclesButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customers";
+            // 
+            // customersSearch
+            // 
+            this.customersSearch.Location = new System.Drawing.Point(379, 30);
+            this.customersSearch.Name = "customersSearch";
+            this.customersSearch.Size = new System.Drawing.Size(142, 20);
+            this.customersSearch.TabIndex = 1;
+            this.customersSearch.TextChanged += new System.EventHandler(this.customersSearch_TextChanged);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(413, 56);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // workButton
+            // 
+            this.workButton.Location = new System.Drawing.Point(413, 339);
+            this.workButton.Name = "workButton";
+            this.workButton.Size = new System.Drawing.Size(75, 36);
+            this.workButton.TabIndex = 3;
+            this.workButton.Text = "Work Done";
+            this.workButton.UseVisualStyleBackColor = true;
+            this.workButton.Click += new System.EventHandler(this.workButton_Click);
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataMember = "Customers";
+            // 
+            // saveButton1
+            // 
+            this.saveButton1.Location = new System.Drawing.Point(32, 339);
+            this.saveButton1.Name = "saveButton1";
+            this.saveButton1.Size = new System.Drawing.Size(75, 23);
+            this.saveButton1.TabIndex = 7;
+            this.saveButton1.Text = "Add";
+            this.saveButton1.UseVisualStyleBackColor = true;
+            this.saveButton1.Click += new System.EventHandler(this.saveButton1_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(39, 131);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 20);
+            this.nameBox.TabIndex = 8;
+            // 
+            // areaBox
+            // 
+            this.areaBox.Location = new System.Drawing.Point(210, 131);
+            this.areaBox.Name = "areaBox";
+            this.areaBox.Size = new System.Drawing.Size(100, 20);
+            this.areaBox.TabIndex = 9;
+            // 
+            // addressBox
+            // 
+            this.addressBox.Location = new System.Drawing.Point(39, 193);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(100, 20);
+            this.addressBox.TabIndex = 10;
+            // 
+            // phoneBox
+            // 
+            this.phoneBox.Location = new System.Drawing.Point(210, 193);
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneBox.TabIndex = 11;
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(123, 244);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(100, 20);
+            this.emailBox.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "City, State, Zip";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(240, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Phone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(155, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Email";
+            // 
+            // customersBindingSource1
+            // 
+            this.customersBindingSource1.DataMember = "Customers";
+            this.customersBindingSource1.DataSource = this.customersDataSet;
+            // 
+            // customersDataSet
+            // 
+            this.customersDataSet.DataSetName = "customersDataSet";
+            this.customersDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CustomersTableAdapter = this.customersTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Joes_Automotive.customersDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(113, 339);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 21;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 381);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(476, 150);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(194, 339);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 23;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // vehiclesButton
+            // 
+            this.vehiclesButton.Location = new System.Drawing.Point(413, 297);
+            this.vehiclesButton.Name = "vehiclesButton";
+            this.vehiclesButton.Size = new System.Drawing.Size(75, 36);
+            this.vehiclesButton.TabIndex = 24;
+            this.vehiclesButton.Text = "Vehicles";
+            this.vehiclesButton.UseVisualStyleBackColor = true;
+            this.vehiclesButton.Click += new System.EventHandler(this.vehiclesButton_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 661);
+            this.Controls.Add(this.vehiclesButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.phoneBox);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.areaBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.saveButton1);
+            this.Controls.Add(this.workButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.customersSearch);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.showCustomersButton_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox customersSearch;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button workButton;
+        private System.Windows.Forms.BindingSource customersBindingSource;
+        private System.Windows.Forms.Button saveButton1;
+        private customersDataSet customersDataSet;
+        private System.Windows.Forms.BindingSource customersBindingSource1;
+        private customersDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        private customersDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox areaBox;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.TextBox phoneBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button vehiclesButton;
+    }
+}
---
+++ Joes Automotive/Joes Automotive/Form3.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joes_Automotive
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        
+        DataTable table = new DataTable();
+        int selectedRow;
+        
+        private void workButton_Click(object sender, EventArgs e)
+        {
+            Form4 f4 = new Form4();
+            f4.ShowDialog();
+        }
+
+        private void customersDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void showCustomersButton_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'customersDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.customersDataSet.Customers);
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("City, State, Zip", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("Phone", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+
+            table.Rows.Add("Anthony Green", "Bay City, MI, 48706", "600 westlawn st", "9894506656", "agreen_2001@yahoo.com");
+            table.Rows.Add("Billy Bob", "Melbourne, FL, 39204", "123 6th st", "2515469442", "billbob_4557@yahoo.com");
+            table.Rows.Add("Matt Tress", "Chevy Chase, MD, 20815", "71 Pilgrim Avenue", "1255464478", "tress_4573@yahoo.com");
+            table.Rows.Add("John Smith", "South Windsor, CT, 06074", "70 Bowman St", "9495694371", "jsmith_2001@yahoo.com");
+
+            dataGridView1.DataSource = table;
+
+        }
+
+        private void saveButton1_Click(object sender, EventArgs e)
+        {
+           table.Rows.Add(nameBox.Text, areaBox.Text, addressBox.Text, phoneBox.Text, emailBox.Text);
+            dataGridView1.DataSource = table;
+
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[selectedRow];
+            nameBox.Text = row.Cells[0].Value.ToString();
+            areaBox.Text = row.Cells[1].Value.ToString();
+            addressBox.Text = row.Cells[2].Value.ToString();
+            phoneBox.Text = row.Cells[3].Value.ToString();
+            emailBox.Text = row.Cells[4].Value.ToString();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+
+            DataGridViewRow newDataRow = dataGridView1.Rows[selectedRow];
+            newDataRow.Cells[0].Value = nameBox.Text;
+            newDataRow.Cells[1].Value = areaBox.Text;
+            newDataRow.Cells[2].Value = addressBox.Text;
+            newDataRow.Cells[3].Value = phoneBox.Text;
+            newDataRow.Cells[4].Value = emailBox.Text;
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            selectedRow = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(selectedRow);
+        }
+
+        private void vehiclesButton_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e) 
+        {
+           if (string.IsNullOrEmpty(customersSearch.Text))
+            {
+                (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Empty;
+            }            
+           else
+            {
+                (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Format("Name='{0}'", customersSearch.Text);
+            }
+        }
+
+        private void customersSearch_TextChanged(object sender, EventArgs e)
+        {
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Format("Name LIKE '{0}%'", customersSearch.Text);
+        }
+    }
+}
